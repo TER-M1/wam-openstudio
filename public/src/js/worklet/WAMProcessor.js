b75f4849d6aa93572dcfa7b959b3e2de44724bd6
Antoine Vidal-Mazuy <antoine.vidalma@gmail.com>
2022-06-17 10:24:03 +0200

    Clear events before apply the new ones

--- public/src/js/worklet/WAMProcessor.js
+++ public/src/js/worklet/WAMProcessor.js
@@ -161,7 +161,6 @@ const getProcessor = (moduleId) => {
                 }
                 console.log(events);
                 this.emitEvents(...events);
-                // this.scheduleEvents(...events);
             }
         }
 
